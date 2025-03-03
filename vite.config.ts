f3b891b1131c826199dc3dd93fa26b7cf87a296e
slogvo <86196042+slogvo@users.noreply.github.com>
2025-03-03 14:08:47 +0700

    feat: enhance project structure with TypeScript configuration, add build scripts, and implement continuous deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { dirname, resolve } from "path";
 import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath } from "url";
+import dts from "vite-plugin-dts";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), dts({
+      insertTypesEntry: true,
+      outDir: "dist",
+      include: ["src"],
+    }),],
     server: {
       port: 3000,
       open: true,
@@ -21,7 +30,24 @@ export default defineConfig(({ mode }) => {
     },
     resolve: {
       alias: {
-        "@": path.resolve(__dirname, "./src"),
+        "@": resolve(__dirname, "./src"),
+      },
+    },
+    build: {
+      sourcemap: true,
+      lib: {
+        entry: resolve(__dirname, "src/index.ts"),
+        name: "VideoPlayer",
+        fileName: (format) => `index.${format === "es" ? "mjs" : "js"}`,
+      },
+      rollupOptions: {
+        external: ["react", "react-dom"],
+        output: {
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
+        },
       },
     },
   };
